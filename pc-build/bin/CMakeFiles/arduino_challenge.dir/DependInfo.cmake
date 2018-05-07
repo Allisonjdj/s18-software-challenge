64f08af22c5783d543d80f069c649f8fd79d9277
Deep Dhillon <deep@deepdhillon.ca>
2018-05-07 09:12:58 -0400

    added back pc-build

---
+++ pc-build/bin/CMakeFiles/arduino_challenge.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/deep/Development/waterloop/arduino-challenge/src/SimplexNoise.cpp" "/Users/deep/Development/waterloop/arduino-challenge/pc-build/bin/CMakeFiles/arduino_challenge.dir/Users/deep/Development/waterloop/arduino-challenge/src/SimplexNoise.cpp.o"
+  "/Users/deep/Development/waterloop/arduino-challenge/src/genfunc.cpp" "/Users/deep/Development/waterloop/arduino-challenge/pc-build/bin/CMakeFiles/arduino_challenge.dir/Users/deep/Development/waterloop/arduino-challenge/src/genfunc.cpp.o"
+  "/Users/deep/Development/waterloop/arduino-challenge/src/main.cpp" "/Users/deep/Development/waterloop/arduino-challenge/pc-build/bin/CMakeFiles/arduino_challenge.dir/Users/deep/Development/waterloop/arduino-challenge/src/main.cpp.o"
+  "/Users/deep/Development/waterloop/arduino-challenge/pc-build/main.cpp" "/Users/deep/Development/waterloop/arduino-challenge/pc-build/bin/CMakeFiles/arduino_challenge.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "MAX_ALLOCATORS=10u"
+  "NUM_BLOCKS=400u"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../pkg/wlib"
+  "../mock-cosa"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/deep/Development/waterloop/arduino-challenge/pc-build/bin/CMakeFiles/cosa.dir/DependInfo.cmake"
+  "/Users/deep/Development/waterloop/arduino-challenge/pc-build/bin/CMakeFiles/wlib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ pc-build/bin/CMakeFiles/cosa.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cosa.dir/mock-cosa/Cosa/RTT.cpp.o"
+  "CMakeFiles/cosa.dir/mock-cosa/Cosa/Trace.cpp.o"
+  "CMakeFiles/cosa.dir/mock-cosa/Cosa/UART.cpp.o"
+  "libcosa.pdb"
+  "libcosa.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cosa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
